c372208b339f1803d49d554425b81d132dd5364b
Pavan Kumar <pavan2.kumar@intel.com>
2022-02-03 15:04:56 +0530

    test  $
    git commit -m "Demo

---
+++ WebApplication2/Services/IShipmentService.cs
@@ -0,0 +1,10 @@
+﻿using static WebApplication2.Services.Models;
+
+namespace WebApplication2.Services
+{
+    public interface IShipmentService
+    {
+        void Ship(IAddressInfo info, IEnumerable<CartItem> items);
+
+    }
+}
